3bda0655acacdea6ec85466644005a85f08dd1a4
woaitsAryan <yoprince.ab@gmail.com>
2024-09-28 21:50:53 +0530

    feat: added repo's

--- lib/repository.ts
+++ lib/repository.ts
@@ -6,28 +6,82 @@ interface repoInterface {
 }
 
 export const repositories: repoInterface[] = [
+	{
+		name: "News Classifier",
+		tech: "Python, Jupyter notebook",
+		url: "https://github.com/csivitu/news-classifier",
+		numOfIssues: 8,
+	},
+	{
+		name: "Card Game",
+		tech: "HTML, CSS and JS",
+		url: "https://github.com/csivitu/card-game",
+		numOfIssues: 0,
+	},
+	{
+		name: "Neo S",
+		tech: "Python, Numpy",
+		url: "https://github.com/csivitu/neo-s",
+		numOfIssues: 1,
+	},
+	{
+		name: "Maya",
+		tech: "Python",
+		url: "https://github.com/csivitu/maya",
+		numOfIssues: 8,
+	},
+	{
+		name: "Go-sail",
+		tech: "Golang",
+		url: "https://github.com/csivitu/go-sail",
+		numOfIssues: 0,
+	},
+	{
+		name: "Snake vs background Classifier",
+		tech: "Python, jupyter notebook",
+		url: "https://github.com/csivitu/snake-spotter",
+		numOfIssues: 8,
+	},
+	{
+		name: "Vigilant Brocoli",
+		tech: "Python",
+		url: "https://github.com/csivitu/vigilant-brocoli",
+		numOfIssues: 8,
+	},
+	{
+		name: "Discord Bot",
+		tech: "Python",
+		url: "https://github.com/csivitu/discord-bot",
+		numOfIssues: 10,
+	},
+	{
+		name: "Covert Messenger",
+		tech: "Python, Bash script",
+		url: "https://github.com/csivitu/stealth-chat",
+		numOfIssues: 10,
+	},
+	{
+		name: "Student Database",
+		tech: "Java",
+		url: "https://github.com/csivitu/student-database",
+		numOfIssues: 10,
+	},
+	{
+		name: "Http server",
+		tech: "C",
+		url: "https://github.com/csivitu/http-server",
+		numOfIssues: 7,
+	},
+	{
+		name: "Fireman Game",
+		tech: "Javascript, HTML, CSS",
+		url: "https://github.com/csivitu/fireman",
+		numOfIssues: 7,
+	},
     {
-        name: "Fireman",
-        tech: "JavaScript, React, CSS, HTML",
-        numOfIssues: 10,
-        url: "http://github.com/woaitsAryan"
-    },
-    {
-        name: "Fireman",
-        tech: "JavaScript, React, CSS, HTML",
-        numOfIssues: 10,
-        url: "http://github.com/woaitsAryan"
-    },
-    {
-        name: "Fireman",
-        tech: "JavaScript, React, CSS, HTML",
-        numOfIssues: 10,
-        url: "http://github.com/woaitsAryan"
-    },
-    {
-        name: "Fireman",
-        tech: "JavaScript, React, CSS, HTML",
-        numOfIssues: 10,
-        url: "http://github.com/woaitsAryan"
-    },
-]
+		name: "Snakes & Ladders",
+		tech: "Javascript, HTML, CSS",
+		url: "https://github.com/csivitu/fireman",
+		numOfIssues: 7,
+	},
+];
