e29c64b22e5f1edf866027ccdd547cbac993540a
woaitsAryan <yoprince.ab@gmail.com>
2024-09-28 18:48:43 +0530

    feat: integrated leaderboarD

--- app/components/LeaderboardBox.tsx
+++ app/components/LeaderboardBox.tsx
@@ -1,6 +1,12 @@
+import type { leaderboardType } from "@/lib/types";
+import Link from "next/link";
 import React from "react";
 
-const LeaderboardBox = () => {
+interface LeaderboardBoxProps {
+  leaderboardDate: leaderboardType[] | null
+}
+
+const LeaderboardBox = (props: LeaderboardBoxProps) => {
   return (
     <div className="w-[90%] md:w-[60%] h-[80vh] flex flex-col  gap-4  z-100 absolute right-1/2 translate-x-1/2 md:translate-x-0 md:right-5 top-5">
       <h1 className="text-6xl font-robotoslab font-bold text-center">
@@ -12,22 +18,23 @@ const LeaderboardBox = () => {
             Rank
           </span>
           <span className="bg-[#494B56] px-4 py-2 rounded-lg text-2xl font-robotomono font-semibold text-center">
-            Team Name
+            Name
           </span>
           <span className="bg-[#494B56] px-4 py-2 rounded-lg text-2xl font-robotomono font-semibold text-center">
             Score
           </span>
         </div>
         <div className="flex flex-col gap-4 h-[70vh] overflow-y-auto mt-4 inner-scroller">
-          {Array.from({ length: 10 }).map((team, index) => (
-            <div
+          {props.leaderboardDate?.map((user) => (
+            <Link
+              href={`https://github.com/${user.githubUsername}`}
               className="bg-[#494B56] flex items-center mr-4 font-semibold font-robotomono justify-around py-4 rounded-lg"
-              key={index}
+              key={user.githubUsername}
             >
-              <h1>1</h1>
-              <h1>Code Crafter</h1>
-              <h1>431</h1>
-            </div>
+              <h1>{user.rank}</h1>
+              <h1>{user.githubUsername}</h1>
+              <h1>{user.score}</h1>
+            </Link>
           ))}
         </div>
       </div>
