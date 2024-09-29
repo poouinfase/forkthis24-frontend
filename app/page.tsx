c33ddc677f54f4002a429b7303f6a74824ec7b95
Keshav-Aneja <keshav.aneja09@gmail.com>
2024-09-29 10:45:14 +0530

    feat: smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
+"use client";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
+import { ReactLenis } from "@studio-freight/react-lenis";
+
 import Timeline2 from "./components/Timeline2";
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Timeline2 />
-      {/* <Timeline /> */}
-      <Faq />
-      <Footer />
-    </div>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.04,
+        duration: 1.5,
+        smoothWheel: true,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: "vertical",
+      }}
+    >
+      <div>
+        <Navbar />
+        <Hero />
+        <Timeline2 />
+        {/* <Timeline /> */}
+        <Faq />
+        <Footer />
+      </div>
+    </ReactLenis>
   );
 }
